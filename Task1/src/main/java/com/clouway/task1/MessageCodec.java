68273b47fd3a65901f4c412949dc45f5afa88164
Borislav Gadjev <gadjevb@gmail.com>
2016-07-13 12:36:13 +0300

    Added documentation for the MessageCodec interface!

--- Task1/src/main/java/com/clouway/task1/MessageCodec.java
+++ Task1/src/main/java/com/clouway/task1/MessageCodec.java
@@ -4,6 +4,21 @@ import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
 public interface MessageCodec {
-    void marshal(Object object, String url) throws JAXBException, IOException;
-    Object unmarshal(Class type, String url) throws JAXBException, IOException;
+    /**
+     * This method receives an object for marshalling and url path to the receiving file as params.
+     *
+     * @param object
+     * @param url
+     */
+    void marshal(Object object, String url);
+
+    /**
+     * This method receives Class type (like Person.class) and url path to the file with the info that must be extracted.
+     * The user should cast the returned value to the desired class.
+     *
+     * @param type
+     * @param url
+     * @return
+     */
+    Object unmarshal(Class type, String url);
 }
