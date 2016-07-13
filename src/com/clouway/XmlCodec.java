258b0c8392d0022f57ef13566521aae354262460
Vasil <v.mitov.clouway@gmail.com>
2016-07-13 15:59:03 +0300

    Maven build is ready.

--- src/com/clouway/XmlCodec.java
+++ src/com/clouway/XmlCodec.java
@@ -5,13 +5,13 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import java.io.ByteArrayInputStream;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
-import static javafx.scene.input.KeyCode.T;
+
 
 /**
  * @author Vasil Mitov (v.mitov.clouway@gmail.com)
