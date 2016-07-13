43e5232621d50e24e6a3b3ca9b3dcab735019a5e
Borislav Gadjev <gadjevb@gmail.com>
2016-07-13 15:41:10 +0300

    The main class in MessageCodec was wrong, this is the fix for it!

--- Task2/src/main/java/com/clouway/task2/MessagePreview.java
+++ Task2/src/main/java/com/clouway/task2/MessagePreview.java
@@ -4,12 +4,23 @@ import com.clouway.task1.JSONCodecConverter;
 
 public class MessagePreview {
 
+    public static void printAverageStats(User[] users){
+        int counter = 0, temp = 0;
+        double age;
+        for (User each: users) {
+            counter++;
+            temp = temp + each.getAge();
+        }
+        age = (temp / counter);
+        System.out.print("Users Count: " + counter + "\n" + "Average Age: " + age + "\n");
+    }
+
     public static void main(String [] args){
         JSONCodecConverter jsonConverter = new JSONCodecConverter();
 
         if(args[0].equals("-type") && args[1].equals("json") && args[3].equals("--printAverageStats")){
-            Users users = (Users) jsonConverter.unmarshal(Users.class, args[2]);
-            users.printAverageStats();
+            User[] users = (User[]) jsonConverter.unmarshal(User[].class, args[2]);
+            printAverageStats(users);
         }
     }
 }
